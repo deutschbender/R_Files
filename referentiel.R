1552bb3a15184f3a8778d6474a5c6eb286b8ddbc
deutschbender <jvanneau@gmail.com>
2020-01-15 10:52:26 +0100

    Update referentiel.R
    add a version of multi write

--- referentiel.R
+++ referentiel.R
@@ -1,3 +1,6 @@
 
 ## write multiple file based on a column value
+mydata[, fwrite(.SD, paste0("output", var1,".csv")), by = var1]
+
+# duplicate var1 to keep in the output data file
 mydata[, fwrite(copy(.SD)[, var1 := var1] paste0("output", var1,".csv")), by = var1]
