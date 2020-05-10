dd6032904b6dadaf2b3eafdbeb3b1a06b125b4b0
deutschbender <jvanneau@gmail.com>
2020-05-10 19:29:14 +0200

    Update datacamp__unsupervised-learning-in-r.R

--- datacamp__unsupervised-learning-in-r.R
+++ datacamp__unsupervised-learning-in-r.R
@@ -3,3 +3,6 @@ km.out <- kmeans(x, centers = 3, nstart = 20)
 
 # Inspect the result
 summary(km.out)
+
+# Scatter plot of x
+plot(x, col = km.out$cluster, main = "k-means with 3 clusters", xlab = "", ylab = "")
