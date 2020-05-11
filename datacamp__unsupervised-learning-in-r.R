bb2f8515a49d2c0ad5179b2489240f87b9846c27
deutschbender <jvanneau@gmail.com>
2020-05-11 12:57:06 +0200

    Update datacamp__unsupervised-learning-in-r.R

--- datacamp__unsupervised-learning-in-r.R
+++ datacamp__unsupervised-learning-in-r.R
@@ -82,3 +82,77 @@ hclust.out <- hclust(dist(x))
 
 # Inspect the result
 summary(hclust.out)
+
+# Cut by height
+cutree(hclust.out, h = 7)
+
+# Cut by number of clusters
+cutree(hclust.out, k = 3)
+
+# Cluster using complete linkage: hclust.complete
+hclust.complete <- hclust(dist(x), method = "complete")
+
+# Cluster using average linkage: hclust.average
+hclust.average <- hclust(dist(x), method = "average")
+
+# Cluster using single linkage: hclust.single
+hclust.single <- hclust(dist(x), method = "single")
+
+# Plot dendrogram of hclust.complete
+plot(hclust.complete, main = "Complete")
+
+# Plot dendrogram of hclust.average
+plot(hclust.average, main = "Average")
+
+# Plot dendrogram of hclust.single
+plot(hclust.single, main = "Single")
+
+# View column means
+colMeans(pokemon)
+
+# View column standard deviations
+apply(pokemon, 2, sd)
+
+# Scale the data
+pokemon.scaled <- scale(pokemon)
+
+# Create hierarchical clustering model: hclust.pokemon
+hclust.pokemon <- hclust(dist(pokemon.scaled), method = "complete")
+
+# Apply cutree() to hclust.pokemon: cut.pokemon
+cut.pokemon <- cutree(hclust.pokemon, k = 3)
+
+
+# Compare methods
+table(cut.pokemon, km.pokemon$cluster)
+
+
+#### CHAPTER 3 - PRINCIPAL COMPONENT ANALYSIS ####
+##################################################
+
+# Perform scaled PCA: pr.out
+pr.out <- prcomp(x = pokemon, scale = TRUE, center = TRUE) 
+
+# Inspect model output
+summary(pr.out)
+
+
+##Visualize biplot
+biplot(pr.out)
+
+
+# Variability of each principal component: pr.var
+pr.var <- pr.out$sdev^2
+
+# Variance explained by each principal component: pve
+pve <- pr.var / sum(pr.var)
+
+# Plot variance explained for each principal component
+plot(pve, xlab = "Principal Component",
+     ylab = "Proportion of Variance Explained",
+     ylim = c(0, 1), type = "b")
+
+# Plot cumulative proportion of variance explained
+plot(cumsum(pve), xlab = "Principal Component",
+     ylab = "Cumulative Proportion of Variance Explained",
+     ylim = c(0, 1), type = "b")
